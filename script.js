ad8d6ab6ed6e4be0fdb4c14655ca3398985dbd36
Ismail Hilali <bilouge@hotmail.fr>
2020-06-15 19:44:09 -0400

    Added table animations and better slide transition animations

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Go to the top of the page when document loads
 $(document).ready(() => {
     $(this).scrollTop(0);
-})
+});
     
 
-const gamePos = $('#game').offset().top;
 let pastScroll = $(document).scrollTop();
 let currentPos = 0;
 let canScroll = true;
@@ -13,19 +12,46 @@ const slides = [
         name: "#intro"
     },
     {
-        name: "#game"
+        name: "#table-of-content"
     }
 ];
 
+// Populate "slides" with position of objects
 slides.forEach(item => item.pos = $(item.name).offset().top);
+    
 
 const scrollToSlide = (slide) => {
-    console.log("trigger")
-    $("html, body").animate({scrollTop: slide.pos}, 1000);
-    setTimeout(() => canScroll = true, 1100);
+    
+    //Can't scroll during animation...
+    $('body').css('overflow', 'hidden');
+
+    if(currentPos != 1 ) {
+        $('.big-table').removeClass("big-table");
+        $('.table').addClass("small-table");
+        setTimeout(() => {
+            $('.table').addClass( "hover-active" );
+            $('#table-tooltip').css('display', 'inline');
+        }, 1000);
+    }
+    else {
+        $('.hover-active').removeClass( "hover-active" );
+        $('#table-tooltip').css('display', 'none');
+    }
+    $("html, body").animate({scrollTop: slide.pos}, 1000, () => {
+        if(currentPos == 1){
+            $('.table').addClass("big-table");
+            $('.small-table').removeClass("small-table");
+        }
+        setTimeout(() => {
+            canScroll = true;
+            //... Can scroll again once animation is done
+            $('body').css('overflow', 'visible');
+        }, 1100);
+    });
 };
 
-console.log(currentPos);
+
+
 
 window.addEventListener('scroll', () => {
     let currentScroll = $(document).scrollTop();
@@ -34,7 +60,6 @@ window.addEventListener('scroll', () => {
     if(canScroll) {
         canScroll = false;
 
-        console.log($('.indicator')[currentPos]);
         $('.indicator').removeClass( "current" );
 
         // If scroll down
